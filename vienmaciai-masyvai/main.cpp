cdb5c6883e7a46a2f137bbe278f96a0958ab086d
JustinasZailskas <justinaszailskas@gmail.com>
2026-02-24 11:01:35 +0200

    Paprastas masyvas su informacijos isvedimu

---
+++ vienmaciai-masyvai/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int skaiciai[5];
+    // skaiciai[3] = 15;
+    // skaiciai[0] = 4;
+    // skaiciai[1] = 5;
+    // skaiciai[2] = skaiciai[0] + skaiciai[1];
+    // skaiciai[4] = skaiciai[3] / skaiciai[1];
+    // cout<<skaiciai[0]<<endl;
+    // cout<<skaiciai[1]<<endl;
+    // cout<<skaiciai[2]<<endl;
+    // cout<<skaiciai[3]<<endl;
+    // cout<<skaiciai[4]<<endl;
+
+    // for(int i = 0; i < 5; i++) {
+    //     cout <<"Masyvo "<<i+1<<" elemento reiksme: "<<skaiciai[i]<<endl;
+    // }
+
+
+
+    return 0;
+}
