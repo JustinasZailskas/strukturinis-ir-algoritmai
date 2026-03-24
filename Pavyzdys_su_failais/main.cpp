00b6bf9bc0bb168e6c1363d42d1453560eaec95d
JustinasZailskas <justinaszailskas@gmail.com>
2026-03-24 11:05:20 +0200

    Apskaiciuota informacija saugojama faile

--- Pavyzdys_su_failais/main.cpp
+++ Pavyzdys_su_failais/main.cpp
@@ -6,6 +6,9 @@ using namespace std;
 int main() {
 
     int masyvasSkaiciu[15];
+    int kiekis = 0;
+    int suma = 0;
+    int vidurkis = 0;
 
     ifstream duomenuFailas;
     duomenuFailas.open("duomenys.txt");
@@ -18,8 +21,12 @@ int main() {
         cout <<"Failas atidarytas"<<endl;
     }
 
-    for (int i = 0; i < 8; i++) {
-        duomenuFailas >> masyvasSkaiciu[i];
+    // for (int i = 0; i < 8; i++) {
+    //     duomenuFailas >> masyvasSkaiciu[i];
+    // }
+    while(!duomenuFailas.eof()) {
+        duomenuFailas >> masyvasSkaiciu[kiekis];
+        kiekis++;
     }
     cout<<"Duomenys nuskaityti"<<endl;
     duomenuFailas.close();
@@ -27,15 +34,30 @@ int main() {
         cout <<"Failas uzdarytas"<<endl;
     }
 
-
-    for (int i = 0;  i < 8; i++) {
+    for (int i = 0; i < kiekis; i++) {
         cout<<masyvasSkaiciu[i]<<" ";
+        suma += masyvasSkaiciu[i];
     }
 
+    vidurkis = double(suma)/kiekis;
+    cout <<endl;
+    cout << "Suma: "<<suma<<endl;
+    cout << "Vidurkis: "<<vidurkis<<endl;
 
+    ofstream rezultatai;
 
+    rezultatai.open("rezultatai.txt");
 
+    if(rezultatai.fail()) {
+        cout <<"Nepavyko atidaryti rezultatu failo"<<endl;
+        return 1;
+    }
 
+    rezultatai<<"Duomenys faile"<<endl;
+    rezultatai<<"Suma: "<<suma<<endl;
+    rezultatai<<"Vidurkis: "<<vidurkis<<endl;
+
+    rezultatai.close();
 
     return 0;
 }
