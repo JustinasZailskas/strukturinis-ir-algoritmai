2a2481e62eac1eca185f2b6a1f992f122f7aa419
JustinasZailskas <justinaszailskas@gmail.com>
2026-04-14 13:31:04 +0300

    Pabaigtas sifravimo/desifravimo algoritmas. Truputi atliktas kodo refaktorinimas.

--- cezario-algoritmas/main.cpp
+++ cezario-algoritmas/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 char abecele[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
+string sifravimas(string tekstas, int poslinkis, string tipas);
 
 int main() {
     string tekstas; //tekstas, kuri noresiu uzsifruoti
@@ -33,21 +34,27 @@ int main() {
             case 1:
                 cout << "Uzsifruoti \n";
                 cout << "Iveskite norima uzsifruoti teksta: "<<endl;
-                cin >> tekstas;
+                cin.ignore(10000, '\n');
+                getline(cin, tekstas);
                 cout << "Iveskite poslinki: "<<endl;
                 cin >> poslinkis;
 
                 for(int i = 0; i < tekstas.length(); i++) {
                     tekstas[i] = toupper(tekstas[i]);
                 }
-
+                cout << "Uzsifruotas tekstas: " <<sifravimas(tekstas, poslinkis, tipas)<<endl;
                 break;
             case 2:
                 cout<<"Desifruoti \n";
                 cout<<"Iveskite norima desifruoti teksta: "<<endl;
-                cin>>tekstas;
+                cin.ignore(10000, '\n');
+                getline(cin, tekstas);
                 cout << "Iveskite poslinki: "<<endl;
                 cin >> poslinkis;
+                for(int i = 0; i < tekstas.length(); i++) {
+                    tekstas[i] = toupper(tekstas[i]);
+                }
+                cout << "Desifruotas tekstas: " <<sifravimas(tekstas, poslinkis, tipas)<<endl;
                 break;
             default:
                 cout << "Neteisinga ivestis \n";
@@ -62,21 +69,22 @@ string sifravimas(string tekstas, int poslinkis, string tipas) {
     int tekstoIndeksai[100];
     int pakeistiTekstoIndeksai[100];
     string rezultatas;
-
+    bool isFind;
     for (int i = 0; i < tekstas.length(); i++) {
+        isFind = false;
         for (int j = 0; j < sizeof(abecele); j++) {
             if (abecele[j] == tekstas[i]) {
                 tekstoIndeksai[i] = j;
+                isFind = true;
             }
         }
-    }
-
-    for (int i = 0; i < tekstas.length(); i++) {
         pakeistiTekstoIndeksai[i] = (tipas == "sifravimas") ?
                                                             (tekstoIndeksai[i] + poslinkis)% sizeof(abecele)
                                                             : (tipas == "desifravimas") ?
                                                                 (tekstoIndeksai[i] - poslinkis + sizeof(abecele))% sizeof(abecele) : 0;
+        rezultatas += isFind ? abecele[pakeistiTekstoIndeksai[i]] : tekstas[i];
+        // rezultatas += tekstas[i];
     }
 
-    // for (int i = 0; i < tekstas)
+    return rezultatas;
 }
