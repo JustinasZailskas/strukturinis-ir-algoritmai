4976dd3bc4c0d3a7350e205099a5b2dc1668145e
JustinasZailskas <justinaszailskas@gmail.com>
2026-04-14 13:31:18 +0300

    Pabaigtas sifravimo/desifravimo algoritmas. Truputi atliktas kodo refaktorinimas.

---
+++ cezario-algoritmas/cmake-build-debug/CMakeFiles/cezario_algoritmas.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cezario_algoritmas.dir/main.cpp.obj"
+  "CMakeFiles/cezario_algoritmas.dir/main.cpp.obj.d"
+  "cezario_algoritmas.exe"
+  "cezario_algoritmas.exe.manifest"
+  "cezario_algoritmas.pdb"
+  "libcezario_algoritmas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cezario_algoritmas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
